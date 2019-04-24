fbad96f20cfc46f53dd331443ffb353f79aa001e
github username <github email address>
2019-04-24 10:52:35 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // FEATURE: when user types into task description field and submits / clicks `Create New Task` button
+  // we want to add their input into the `My Todos` list
+
+  // identify form element
+  const taskForm = document.getElementById('create-task-form');
+  
+  // identify form input
+  const description = document.getElementById('new-task-description');
+
+  // identify element we want to add to
+    // add new li to tasks ul
+  const todoList = document.getElementById('tasks');
+
+  // listen for form submit
+  taskForm.addEventListener('submit', addTodoTask);
+
+  // add input to element
+  function addTodoTask(event){
+    event.preventDefault();
+    // alert('clicked that button');
+    let newTask = document.createElement('li');
+    // console.log(newTask);
+    newTask.innerText = description.value;
+    console.log(newTask);
+    todoList.appendChild(newTask);
+    // clear input field
+    taskForm.reset(); // reset function is available for form elements
+  }
 });
