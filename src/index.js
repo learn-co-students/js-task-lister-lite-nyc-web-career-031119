a2ec4635cf355014e11f56fde8a4b5924d9209ef
danielmartin0126 <danielmartin0126@gmail.com>
2019-04-24 13:57:37 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let description = document.querySelector("#description")
+  let form = document.querySelector("#create-task-form")
+  let list = document.querySelector("#tasks")
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault()
+    let task = description.value
+    let paragraph = document.createElement("li")
+    paragraph.innerText = task
+    list.appendChild(paragraph)
+  })
+
 });
+
