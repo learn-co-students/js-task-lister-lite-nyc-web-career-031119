19881eec1470d562dba6010349cbfac2f2c03fc2
Tommy Lau <tlau425@yahoo.com>
2019-04-24 13:20:41 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.querySelector('#create-task-form')
+  const taskInput = document.querySelector('#new-task-description')
+  const taskButton = document.querySelector('#submit-button')
+
+  // taskButton.addEventListener('click', function(){
+  //     alert('pressed')
+  //   })
+  taskForm.addEventListener('submit', function(e){
+    e.preventDefault()
+
+      const userInput = taskInput.value
+
+      const tasks = document.querySelector("#tasks")
+
+      const newLi = document.createElement('li')
+      newLi.innerText = userInput
+        tasks.appendChild(newLi)
+  })
+
+  // console.log(userInput)
 });
